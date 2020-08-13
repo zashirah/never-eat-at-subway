c75c6bcf4fb0b694d9b0f780218ea1b1f9b86b89
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-13 14:38:02 -0400

    added how to purchase text to products page

--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Products = () => {
   return (
+    <div>
+      <p>To purchase send venmo of the appropriate amount along with the product name to me, Will Shirah, at @Will-Shirah</p>
     <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: "space-around", padding: '50px'}}>
       <div style={{border: 'solid gray 1px', width: '500px'}}>
       <h3>
@@ -17,6 +19,7 @@ const Products = () => {
       <p>front says: "I will never eat at subway again"</p>
       <p>back says: "iwillnevereatatsubwayagain.com"</p>
       </div>
+    </div>
     </div>
   )
 }
